1fa7de0d4cbfd37afec34476575eb61080ad98d2
Kysela Petr <petr@kysela.biz>
2026-06-10 11:38:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='django_serverside_datatable',
-      version='pkv2.2.0',
+      version='2.2.0',
       description="Simple Server-side Datatable processing view for Django",
       long_description=long_description,
       long_description_content_type="text/markdown",
